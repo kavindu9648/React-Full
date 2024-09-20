0f246edc6b3de7f71b35f714eca8d54b21419f20
kavindu9648 <keranga297@gmail.com>
2024-09-21 03:34:54 +0530

    first commit

---
+++ practice/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Welcome to my React App</h1>
+      </div>
+    );
+  }
+}
+export default App;
